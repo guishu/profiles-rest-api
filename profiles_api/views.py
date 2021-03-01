a82f3a1f3203233a3a86ddcaf3a000d7ff2ca1a0
guishu <guillaume@schub.fr>
2021-03-01 14:23:07 +0100

    Added viewset to profiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -44,4 +45,52 @@ class HelloApiView(APIView):
 
     def delete(self, request, pk=None):
         """Handle partial updating an object"""
+        return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API viewser"""
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return an hello message"""
+        a_viewset = [
+            'Uses actions (list, create, retreive, update, partial update)',
+            'Automatically maps to urls using routers',
+            'Provides more functionalities with less code'
+        ]
+
+        return Response({
+            'message': 'Hello',
+            'a_viewset': a_viewset
+        })
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retreive(self, request, pk=None):
+        """handle getting an object by id"""
+        return Response({'method': 'GET'})
+
+    def update(self, request, pk=None):
+        """handle getting an object by id"""
+        return Response({'method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """handle getting an object by id"""
+        return Response({'method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """handle getting an object by id"""
         return Response({'method': 'DELETE'})
